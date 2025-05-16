44b21a303dcd8a016502448a388974af5badd7bf
your_name <your_address_email@example.com>
2025-05-16 22:47:25 +0530

    Table of 5 with printf Done

---
+++ src/Program4_Table5.java
@@ -0,0 +1,37 @@
+public class Program4_Table5 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 1;
+        System.out.println("table of 5 is Created below");
+        System.out.printf("5*1 is %d", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*2 is %d ",n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*3 is %d ",n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*4 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*5 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*6 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*7 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*8 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*9 is %d ", n * i);
+        System.out.println();
+        i = i + 1;
+        System.out.printf("5*10 is %d ", n * i);
+
+
+    }
+}
