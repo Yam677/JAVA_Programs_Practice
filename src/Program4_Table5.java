46ed50942e2c5828ef98879a758aa60a2321d0d7
your_name <your_address_email@example.com>
2025-05-18 01:21:27 +0530

    Table of 5 with printf Done

--- src/Program4_Table5.java
+++ src/Program4_Table5.java
@@ -6,31 +6,31 @@ public class Program4_Table5 {
         System.out.printf("5*1 is %d", n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*2 is %d ",n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*3 is %d ",n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*4 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*5 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*6 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*7 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*8 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*9 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
         System.out.println();
         i = i + 1;
-        System.out.printf("5*10 is %d ", n * i);
+        System.out.printf("%d*%d is %d" ,n,i,n * i);
 
 
     }
